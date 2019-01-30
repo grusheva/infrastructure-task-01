f560415f37e7d26163fec3245949b58d73ae0a4a
maryna_grusheva@epam.com <maryna_grusheva@epam.com>
2019-01-30 19:50:40 +0200

    infrastructure-task-01

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {
+    concat,
+    compact,
+    unique
+} from './array-methods/array-methods';
+
+import {
+    toCamelCase,
+    toKebabCase,
+    toSnakeCase
+} from './string-methods/string-methods';
+
+export {
+    concat,
+    compact,
+    unique,
+    toCamelCase,
+    toKebabCase,
+    toSnakeCase
+}
